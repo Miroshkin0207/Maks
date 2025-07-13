abe8877c5fb6b0991538d76b99e774866900c9dc
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-07-13 11:28:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
     let no = document.querySelector("#no")
     no.checked = false
     if (yes.checked) {
-        text.innerHTML="Умничка, всё верно!"
+        text.innerHTML = "Умничка, всё верно!"
+    }
+    else {
+        text.innerHTMl=""
     }
 }
 function no() {
@@ -14,5 +17,8 @@ function no() {
     yes.checked = false
     if (no.checked) {
         text.innerHTML="Жди докс"
+    }
+    else {
+        text.innerHTMl = ""
     }
 }
