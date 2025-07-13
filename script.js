c596a5e9bd8adf5fb927658802d8fca8e00d0e42
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-07-13 11:13:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿function yes() {
+    let text = document.querySelector("#text")
+    let yes = document.querySelector("#yes")
+    let no = document.querySelector("#no")
+    no.checked = false
+    if (yes.checked) {
+        text.innerHTML="Умничка, всё верно!"
+    }
+}
+function no() {
+    let text = document.querySelector("#text")
+    let yes = document.querySelector("#yes")
+    let no = document.querySelector("#no")
+    yes.checked = false
+    if (no.checked) {
+        text.innerHTML="Жди докс"
+    }
+}
