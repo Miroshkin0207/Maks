fa91431aad2f78332fd65f15ad16a8c4cecb486c
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-07-16 12:10:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-﻿function yes() {
-    let yes = document.querySelector("#yes")
-    let no = document.querySelector("#no")
-    let text = document.querySelector("#text")
-    if (yes.checked) {
-        no.checked = false
-        text.innerHTML = "Умничка, всё верно!"
-    }
-    else {
-        text.innerHTML = ""
-    }
-}
-function no() {
-    let yes = document.querySelector("#yes")
-    let no = document.querySelector("#no")
-    let text = document.querySelector("#text")
-    if (no.checked) {
-        yes.checked = false
-        text.innerHTML = "Жди докс"
-    }
-    else {
-        text.innerHTML = ""
-    }
-}
-function game() {
-    window.location.href = "miroshkin0207.github.io/otziv"
+﻿function back() {
+    window.location.href="miroshkin0207.github.io/maks"
 }
